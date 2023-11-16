968e72be775fc43eb2d5528296393fe9f8b85fd9
habib5634 <hero5276311@gmail.com>
2023-11-16 13:56:43 +0500

    sdfsdf'

--- src/Components/SellingComponents/SellingHeader.js
+++ src/Components/SellingComponents/SellingHeader.js
@@ -2,15 +2,15 @@ import React from 'react'
 
 const SellingHeader = () => {
   return (
-    <div className='px-32 py-20 flex justify-between items-center bg-[#f8f8f8]'>
+    <div className='px-36 py-28 flex justify-between items-center bg-[#f8f8f8]'>
         <div>
 
-            <h1 className='text-[48px] text-[#333333] font-bold'>How to start selling<br/>on Alibaba.com</h1>
-            <h1 className=' text-[#333333] font-bold'>Ready to go global? Your business can penetrate new markets<br/> and reach customers around the world with an Alibaba.com seller<br/> account. Here's how to get started!</h1>
+            <h1 className='text-[48px] text-[#2C2D69]  font-bold'>How to start selling<br/>on Alibaba.com</h1>
+            <h1 className=' text-[#2C2D69] '>Ready to go global? Your business can penetrate new markets<br/> and reach customers around the world with an Alibaba.com seller<br/> account. Here's how to get started!</h1>
            
         </div>
         <div className='  '>
-            <img src='https://s.alicdn.com/@img/imgextra/i2/19999999999999/O1CN01m5ksQi2NjassgInzd_!!19999999999999-2-tps.png' alt='fsd'/>
+            <img src='https://s.alicdn.com/@img/imgextra/i2/19999999999999/O1CN01m5ksQi2NjassgInzd_!!19999999999999-2-tps.png' alt='fsd' className='w-full'/>
         </div>
     </div>
   )
